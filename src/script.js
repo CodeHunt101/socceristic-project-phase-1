105f84a317c1055b87e986fa11fb92dff1708e38
Harold Torres Marino <haroldtm55@gmail.com>
2021-06-14 00:33:58 +1000

    Removed html sample code, added css styles and added league GET request

--- src/script.js
+++ src/script.js
@@ -11,9 +11,10 @@ const apiCongigObj = {
   method: "GET",
   headers: {
     "x-rapidapi-host": "v3.football.api-sports.io",
-    "x-rapidapi-key": "774afab29cc8f4cde7bcb5ccbaf2872a",
+    "x-rapidapi-key": "8bc0ad048d98ed6fa090704ed786ca23",
   },
 };
+//Render list of countries and their flags
 fetch(`https://v3.football.api-sports.io/countries`, apiCongigObj)
   .then((response) => response.json())
   .then((country) => {
@@ -47,3 +48,28 @@ fetch(`https://v3.football.api-sports.io/countries`, apiCongigObj)
       }
     });
   });
+
+const countries = document.getElementsByClassName('country')
+for (const country of countries) {
+  country.addEventListener('click', function(e) {
+    //Show season dropdown
+    document.querySelector('label').style.display = 'inline'
+    document.querySelector('select').style.display = 'inline'
+    fetch(`https://v3.football.api-sports.io/leagues?country=${country.innerText}`,apiCongigObj)
+      .then(resp => resp.json())
+      .then(leagues => {
+        console.log(leagues)
+        //Season dropdown options (if later on I can't upload the table, check values!!)
+        document.getElementById('season').innerHTML = ''
+        leagues.response[0].seasons.forEach(season => document.getElementById('season').appendChild(document.createElement('option')).textContent = season.year)
+        
+        //Parse name and logo of first division league
+        if (!!document.querySelectorAll('.content h3')[0]) {
+          document.querySelectorAll('.content h3')[0].remove()
+          document.querySelectorAll('.content img')[0].remove()
+        }
+        document.querySelector('.content').appendChild(document.createElement('h3')).textContent = leagues.response[0].league.name
+        document.querySelector('.content').appendChild(document.createElement('img')).setAttribute('src', leagues.response[0].league.logo)
+      })
+  })
+}
