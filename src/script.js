a6769b2c285dee92e20e298dc7baf835c8dc0043
Harold Torres Marino <haroldtm55@gmail.com>
2021-06-14 22:03:56 +1000

    Added standings GET request

--- src/script.js
+++ src/script.js
@@ -4,8 +4,8 @@ const countryTargets = [
   "Italy",
   "France",
   "England",
-  "Netherlands",
-  "Brazil",
+  "Portugal",
+  "Belgium",
 ];
 const apiCongigObj = {
   method: "GET",
@@ -18,7 +18,6 @@ const apiCongigObj = {
 fetch(`https://v3.football.api-sports.io/countries`, apiCongigObj)
   .then((response) => response.json())
   .then((country) => {
-    console.log(country.response.length);
     country.response.forEach((country) => {
       for (let i = 0; i < countryTargets.length; i++) {
         if (countryTargets[i] === country.name) {
@@ -47,29 +46,153 @@ fetch(`https://v3.football.api-sports.io/countries`, apiCongigObj)
         }
       }
     });
+    const countries = document.getElementsByClassName("country");
+    for (const country of countries) {
+      country.addEventListener("click", function (e) {
+        //Make sure it deletes all elements from content div except the dropdown
+        let i = document.querySelector(".content").children.length - 1;
+        while (i > 1) {
+          document.querySelector(".content").children[i].remove();
+          i--;
+        }
+        //Show season dropdown
+        document.querySelector("label").style.display = "inline";
+        document.querySelector("select").style.display = "inline";
+        fetch(
+          `https://v3.football.api-sports.io/leagues?country=${country.innerText}`,
+          apiCongigObj
+        )
+          .then((resp) => resp.json())
+          .then((leagues) => {
+            //Season dropdown options (if later on I can't upload the table, check values!!)
+            document.getElementById("season").innerHTML = "";
+            leagues.response[0].seasons
+              .reverse()
+              .forEach(
+                (season) =>
+                  (document
+                    .getElementById("season")
+                    .appendChild(document.createElement("option")).textContent =
+                    season.year)
+              );
+
+            //Render name and logo of first division league
+            if (!!document.querySelectorAll(".content h3")[0]) {
+              document.querySelectorAll(".content h3")[0].remove();
+              document.querySelectorAll(".content img")[0].remove();
+            }
+            document
+              .querySelector(".content")
+              .appendChild(document.createElement("h3")).textContent =
+              leagues.response[0].league.name;
+            document
+              .querySelector(".content h3:first-of-type")
+              .setAttribute("id", leagues.response[0].league.id);
+            document
+              .querySelector(".content")
+              .appendChild(document.createElement("img"))
+              .setAttribute("src", leagues.response[0].league.logo);
+            document
+              .querySelector(".content")
+              .appendChild(document.createElement("h3")).textContent =
+              "1st Place";
+
+            //Render the last champion team logo
+            let leagueId = leagues.response[0].league.id;
+            let season = document.querySelector("select");
+            fetch(
+              `https://v3.football.api-sports.io/standings?league=${leagueId}&season=${season.value}`,
+              apiCongigObj
+            )
+              .then((resp) => resp.json())
+              .then((standings) => {
+                document
+                  .querySelector(".content")
+                  .appendChild(document.createElement("img"))
+                  .setAttribute(
+                    "src",
+                    standings.response[0].league.standings[0][0].team.logo
+                  );
+                //Render Table & Standings last season
+                renderStandingsTable();
+              });
+          });
+      });
+    }
+    //Render champion team logo
+    let season = document.querySelector("select");
+    season.addEventListener("change", function () {
+      if (!!document.querySelectorAll(".content img")[1]) {
+        document.querySelectorAll(".content img")[1].remove();
+      }
+      let leagueId = document.querySelector(".content h3:first-of-type").id;
+      fetch(
+        `https://v3.football.api-sports.io/standings?league=${leagueId}&season=${season.value}`,
+        apiCongigObj
+      )
+        .then((resp) => resp.json())
+        .then((standings) => {
+          document
+            .querySelector(".content")
+            .appendChild(document.createElement("img"))
+            .setAttribute(
+              "src",
+              standings.response[0].league.standings[0][0].team.logo
+            );
+
+          //Render Table & Standings
+          renderStandingsTable();
+        });
+    });
   });
 
-const countries = document.getElementsByClassName('country')
-for (const country of countries) {
-  country.addEventListener('click', function(e) {
-    //Show season dropdown
-    document.querySelector('label').style.display = 'inline'
-    document.querySelector('select').style.display = 'inline'
-    fetch(`https://v3.football.api-sports.io/leagues?country=${country.innerText}`,apiCongigObj)
-      .then(resp => resp.json())
-      .then(leagues => {
-        console.log(leagues)
-        //Season dropdown options (if later on I can't upload the table, check values!!)
-        document.getElementById('season').innerHTML = ''
-        leagues.response[0].seasons.forEach(season => document.getElementById('season').appendChild(document.createElement('option')).textContent = season.year)
-        
-        //Parse name and logo of first division league
-        if (!!document.querySelectorAll('.content h3')[0]) {
-          document.querySelectorAll('.content h3')[0].remove()
-          document.querySelectorAll('.content img')[0].remove()
-        }
-        document.querySelector('.content').appendChild(document.createElement('h3')).textContent = leagues.response[0].league.name
-        document.querySelector('.content').appendChild(document.createElement('img')).setAttribute('src', leagues.response[0].league.logo)
-      })
-  })
+function renderStandingsTable() {
+  // let i=0
+  // while (i>6) {
+  //   document.querySelector('.content').children[i].remove()
+  //   i--
+  // }
+  if (!!document.querySelector("#standings")) {
+    document.querySelector("#standings").remove();
+  }
+  document
+    .querySelector(".content")
+    .appendChild(document.createElement("table"))
+    .setAttribute("id", "standings");
+  document
+    .querySelector("#standings")
+    .appendChild(document.createElement("thead")).className = "table-head";
+  document
+    .querySelector(".table-head")
+    .appendChild(document.createElement("tr")).className = "headers";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "Pos";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "Team";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "P";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "W";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "D";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "L";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "GF";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "GA";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "+/-";
+  document
+    .querySelector(".headers")
+    .appendChild(document.createElement("th")).textContent = "PTS";
 }
